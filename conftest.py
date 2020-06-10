bc9b2319245a655dcda17a8a93dc4fe87c82a891
xialuchao <shylock_xia@126.com>
2020-06-10 17:32:52 +0800

    add multi-account

--- conftest.py
+++ conftest.py
@@ -1,10 +1,12 @@
 import os
 import pytest
 from selenium.webdriver import DesiredCapabilities
-
 from pack.pack_log import logger
 from time import sleep
 from selenium import webdriver
+from page.login_page import loginPage
+
+from data.configyaml import getdata, getThirddata
 from config import config
 
 log = logger()
@@ -13,6 +15,9 @@ log = logger()
 # action：store 存储参数
 # default 参数默认值，此处为“device”
 # help 参数帮助信息，此处为无
+
+
+
 def pytest_addoption(parser):
     parser.addoption("--cmdopt", action="store", default="device", help="None")
 
@@ -41,3 +46,54 @@ def common_browser(cmdopt):
     yield browser
     browser.quit()
     sleep(2)
+
+@pytest.fixture
+def login_browser(cmdopt):
+    # os.system('chcp 65001')
+    # print(cmdopt)
+    if cmdopt == "localhost:5555":
+        # 注意这里5001是selenium grid设置的端口号
+        browser = webdriver.Remote(
+            command_executor='http://localhost:5555/wd/hub',
+            desired_capabilities=DesiredCapabilities.CHROME)
+    elif cmdopt == "192.168.6.218:6666":
+        browser = webdriver.Remote(
+            command_executor='http://192.168.6.174:6666/wd/hub',
+            desired_capabilities=DesiredCapabilities.CHROME)
+    else:
+        raise Exception
+    browser.implicitly_wait(10)
+    browser.get("http://ft1.home.zhaoonline.com/login.shtml?back=")
+    browser.find_element_by_id("loginId").send_keys(getdata("setup_account","username"))
+    browser.find_element_by_id("password").send_keys(getdata("setup_account","password"))
+    browser.find_element_by_id("loginBtn").click()
+    yield browser
+    browser.quit()
+    sleep(2)
+
+@pytest.fixture
+def multi_browser(cmdopt):
+    # os.system('chcp 65001')
+    # print(cmdopt)
+    if cmdopt == "localhost:5555":
+        # 注意这里5001是selenium grid设置的端口号
+        browser = webdriver.Remote(
+            command_executor='http://localhost:5555/wd/hub',
+            desired_capabilities=DesiredCapabilities.CHROME)
+    elif cmdopt == "192.168.6.218:6666":
+        browser = webdriver.Remote(
+            command_executor='http://192.168.6.174:6666/wd/hub',
+            desired_capabilities=DesiredCapabilities.CHROME)
+    else:
+        raise Exception
+    browser.implicitly_wait(10)
+    browser.get("http://ft1.home.zhaoonline.com/login.shtml?back=")
+    for i in range(len(getThirddata("multi_account", 0, "all"))):
+        print(i)
+        browser.find_element_by_id("loginId").send_keys(getThirddata("setup_account", i+1, "username"))
+        browser.find_element_by_id("password").send_keys(getThirddata("setup_account", i+1, "password"))
+        browser.find_element_by_id("loginBtn").click()
+    yield browser
+    browser.quit()
+    sleep(2)
+
---
+++ data/configyaml.py
@@ -0,0 +1,32 @@
+import yaml
+import sys
+sys.path.append(".")
+
+def getalldata():
+    with open("./config.yaml") as f:
+        k = f.read()
+    a = yaml.load(k, Loader=yaml.FullLoader)
+    print(a)
+
+def getdata(config1,config2):
+    with open("./config.yaml") as f:
+        k = f.read()
+    a = yaml.load(k, Loader=yaml.FullLoader)
+    print(a[config1][config2])
+    return a[config1][config2]
+
+# getdata("setup_account", "username")
+
+def getThirddata(first,second,third):
+    with open("./config.yaml") as f:
+        k = f.read()
+    a = yaml.load(k, Loader=yaml.FullLoader)
+    print(a)
+    print("1111111111111111111111111")
+    print(a[first])
+    print("2222222222222222222222222")
+    print(a[first][1])
+    print(a[first][second][third])
+    # return a[config1][config2]
+
+getThirddata("multi_account", 1, "username")
